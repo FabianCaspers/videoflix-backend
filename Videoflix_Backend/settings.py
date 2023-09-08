bdce34248a85a6538a211259479e90f6d79d3faf
FabianCaspers <fabian.caspers1308@gmail.com>
2023-09-08 10:11:07 +0200

    authentication finish

--- Videoflix_Backend/settings.py
+++ Videoflix_Backend/settings.py
@@ -29,6 +29,19 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:4200',
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:4200',
+]
+
+CSRF_COOKIE_NAME = "csrftoken"
+
+CSRF_COOKIE_HTTPONLY = False
+
+CORS_ALLOW_CREDENTIALS = True
+
+
+
+
 CORS_ALLOW_HEADERS = [
     'accept',
     'accept-encoding',
@@ -153,3 +166,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
+}
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+]
+
--- authentication/views.py
+++ authentication/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.contrib.auth.models import User
 from .serializers import UserSerializer
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login as auth_login, logout
 from django.views.decorators.csrf import csrf_exempt
 
 @api_view(['POST'])
@@ -16,16 +16,21 @@ def register(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
-def login_view(request):
+def user_login_view(request):
     if request.method == 'POST':
-        email = request.data['email']
-        password = request.data['password']
-        user = authenticate(request, username=email, password=password)
+        email = request.data.get('email')
+        password = request.data.get('password')
+        username = email.split('@')[0]
+        if not email or not password:
+            return Response({'status': 'Email and password are required'}, status=status.HTTP_400_BAD_REQUEST)
+        user = authenticate(request, username=username, password=password)
+
         if user is not None:
-            login(request, user)
+            auth_login(request, user)
             return Response({'status': 'Logged in'}, status=status.HTTP_200_OK)
         else:
             return Response({'status': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
+
 
 
 @api_view(['GET'])
