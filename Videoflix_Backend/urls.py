aa8a0927cb6f707b87e570f065b17eaa04982b7c
FabianCaspers <fabian.caspers1308@gmail.com>
2023-11-15 20:26:11 +0100

    update

--- Videoflix_Backend/urls.py
+++ Videoflix_Backend/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('authentication/', include('authentication.urls')),
     path('confirm/<str:uidb64>/<str:token>/', views.confirm_email, name='confirm-email'),
-    path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
