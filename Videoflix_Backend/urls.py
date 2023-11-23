0488af0e6836e604885b716891545a778721facf
FabianCaspers <fabian.caspers1308@gmail.com>
2023-11-23 13:03:26 +0100

    add demo videos

--- Videoflix_Backend/urls.py
+++ Videoflix_Backend/urls.py
@@ -3,9 +3,11 @@ from django.urls import path, include
 from authentication import views
 from django.conf import settings
 from django.conf.urls.static import static
+from content.views import VideoListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('authentication/', include('authentication.urls')),
+    path('api/videos/', VideoListView.as_view(), name='video-list'),
     path('confirm/<str:uidb64>/<str:token>/', views.confirm_email, name='confirm-email'),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- content/views.py
+++ content/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Video
 
-# Create your views here.
+class VideoListView(APIView):
+    def get(self, request):
+        videos = Video.objects.all()
+        video_data = [{'title': video.title, 'video_url': video.video_file.url} for video in videos]
+        return Response(video_data)
